a7088340a8d51678d2dc7af31837595be933ffa4
David Corey <dcorey@qualtrics.com>
2018-09-15 10:17:37 -0600

    Rearrange by hierarchy

--- aes.go
+++ aes.go
@@ -4,66 +4,44 @@ import (
 	"encoding/binary"
 )
 
-func ffAdd(a, b byte) byte {
-	return a ^ b
-}
+func cipher(in []byte, w []uint32) []byte {
+	state := toState(in)
 
-func xtime(b byte) byte {
-	// check the 7th bit, if it is set then we will have to reduce after bit shifting
-	shouldReduce := (b & 0x80) != 0
+	Nr := (len(w) - 1) / 4
 
-	a := b << 1
+	state = addRoundKey(state, w[:4])
 
-	if shouldReduce {
-		a = a ^ 0x1b
-	}
+	for i := 1; i <= Nr; i++ {
+		state = subBytes(state)
+		state = shiftRows(state)
 
-	return a
-}
-
-func ffMultiply(a, b byte) byte {
-	result := byte(0x00)
-	mask := byte(0x01)
-	intermediate := a
-
-	for mask > 0 {
-		// mask b to see if it has a bit set at each position
-		if (mask & b) != 0 {
-			// if the bit is set then we want to XOR that position'state intermediate value to the result
-			result = result ^ intermediate
+		if i != Nr {
+			state = mixColumns(state)
 		}
 
-		// calculate a new intermediate based on the old one
-		intermediate = xtime(intermediate)
-
-		// shift the mask to the next position
-		mask = mask << 1
+		state = addRoundKey(state, w[i*4:(i+1)*4])
 	}
 
-	return result
+	return fromState(state)
 }
 
-func subWord(word uint32) uint32 {
-	var output uint32
-	mask := uint32(0x0000000f)
+func inverseCipher(in []byte, w []uint32) []byte {
+	state := toState(in)
 
-	for i := 0; i < 4; i++ {
-		col := word & mask
-		word = word >> 4
+	Nr := (len(w) - 1) / 4
 
-		row := word & mask
-		word = word >> 4
+	state = addRoundKey(state, w[Nr*4:(Nr+1)*4])
 
-		output += uint32(sbox[row][col]) << (8 * uint(i))
+	for round := Nr - 1; round >= 0; round-- {
+		state = invShiftRows(state)
+		state = invSubBytes(state)
+		state = addRoundKey(state, w[round*4:(round+1)*4])
+		if round != 0 {
+			state = invMixColumns(state)
+		}
 	}
 
-	return output
-}
-
-func rotWord(word uint32) uint32 {
-	temp := word >> 24
-	word = word << 8
-	return word + temp
+	return fromState(state)
 }
 
 func keyExpansion(key []byte) []uint32 {
@@ -176,44 +154,66 @@ func invMixColumns(s [][]byte) [][]byte {
 	return sp
 }
 
-func cipher(in []byte, w []uint32) []byte {
-	state := toState(in)
-
-	Nr := (len(w) - 1) / 4
-
-	state = addRoundKey(state, w[:4])
-
-	for i := 1; i <= Nr; i++ {
-		state = subBytes(state)
-		state = shiftRows(state)
-
-		if i != Nr {
-			state = mixColumns(state)
-		}
-
-		state = addRoundKey(state, w[i*4:(i+1)*4])
-	}
-
-	return fromState(state)
+func ffAdd(a, b byte) byte {
+	return a ^ b
 }
 
-func inverseCipher(in []byte, w []uint32) []byte {
-	state := toState(in)
+func xtime(b byte) byte {
+	// check the 7th bit, if it is set then we will have to reduce after bit shifting
+	shouldReduce := (b & 0x80) != 0
 
-	Nr := (len(w) - 1) / 4
+	a := b << 1
 
-	state = addRoundKey(state, w[Nr*4:(Nr+1)*4])
-
-	for round := Nr - 1; round >= 0; round-- {
-		state = invShiftRows(state)
-		state = invSubBytes(state)
-		state = addRoundKey(state, w[round*4:(round+1)*4])
-		if round != 0 {
-			state = invMixColumns(state)
-		}
+	if shouldReduce {
+		a = a ^ 0x1b
 	}
 
-	return fromState(state)
+	return a
+}
+
+func ffMultiply(a, b byte) byte {
+	result := byte(0x00)
+	mask := byte(0x01)
+	intermediate := a
+
+	for mask > 0 {
+		// mask b to see if it has a bit set at each position
+		if (mask & b) != 0 {
+			// if the bit is set then we want to XOR that position'state intermediate value to the result
+			result = result ^ intermediate
+		}
+
+		// calculate a new intermediate based on the old one
+		intermediate = xtime(intermediate)
+
+		// shift the mask to the next position
+		mask = mask << 1
+	}
+
+	return result
+}
+
+func subWord(word uint32) uint32 {
+	var output uint32
+	mask := uint32(0x0000000f)
+
+	for i := 0; i < 4; i++ {
+		col := word & mask
+		word = word >> 4
+
+		row := word & mask
+		word = word >> 4
+
+		output += uint32(sbox[row][col]) << (8 * uint(i))
+	}
+
+	return output
+}
+
+func rotWord(word uint32) uint32 {
+	temp := word >> 24
+	word = word << 8
+	return word + temp
 }
 
 func makeEmptyState() [][]byte {
